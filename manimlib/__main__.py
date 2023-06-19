3b63c8fa434a824e3c8271571766bba969e3fe61
slowisfast <slowisfast2030@163.com>
2023-06-19 18:35:26 +0800

    all is well

---
+++ manimlib/__main__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from manimlib import __version__
+import manimlib.config
+import manimlib.extract_scene
+import manimlib.logger
+import manimlib.utils.init_config
+
+from manimlib.logger import log
+
+def main():
+    print(f"ManimGL \033[32mv{__version__}\033[0m")
+
+    args = manimlib.config.parse_cli()
+    if args.version and args.file is None:
+        return
+    if args.log_level:
+        manimlib.logger.log.setLevel(args.log_level)
+
+    if args.config:
+        manimlib.utils.init_config.init_customization()
+    else:
+        config = manimlib.config.get_configuration(args)
+        scenes = manimlib.extract_scene.main(config)
+
+        log.info(f"\n===scenes: {scenes}\n")
+        for scene in scenes:
+            scene.run()
+            log.info('\nall is well')
+
+
+if __name__ == "__main__":
+    main()    
---
+++ start.py
@@ -0,0 +1,38 @@
+from manimlib import *
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(BLUE, opacity=0.5)
+        circle.set_stroke(BLUE_E, width=4)
+        square = Square()
+
+        self.play(ShowCreation(square))
+        self.wait()
+        self.play(ReplacementTransform(square, circle))
+        self.wait()
+
+"""
+安装了两个manimgl环境
+manimgl：初始化conda环境后，下载了manimgl的源码，执行python -m pip install -e .
+manimgl_3b1b：初始化conda环境后，执行python -m pip install manimgl
+
+manimgl环境下执行manim start.py SquareToCircle会报错
+manimgl_3b1b环境下执行manim start.py SquareToCircle会报错
+
+vscode选中manimgl环境，变量不能跳转
+vscode选中manimgl_3b1b环境，变量可以跳转
+
+头疼。。。
+
+cd /opt/anaconda3/envs/manimgl/lib/python3.9/site-packages
+可以发现没有manimlib文件夹，但是manimlib_3b1b环境下有这个文件夹
+这可能就是两种安装方式的区别
+
+====
+终于找到了原因：
+根源在于python -m pip install -e .
+-e的意思是editable，即可编辑的，这种安装方式会在site-packages目录下创建一个egg-link文件，指向源码目录
+
+下载的源码路径：/Users/linus/Desktop/less-is-more/3b1b_anaconda_install/manim
+"""
