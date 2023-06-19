39c9bd6d1eeb1fc1bc25eea6e525dd6aa7afb419
slowisfast <slowisfast2030@163.com>
2023-06-19 21:48:46 +0800

    all is well

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ from manimlib import *
 class SquareToCircle(Scene):
     def construct(self):
         circle = Circle()
-        circle.set_fill(BLUE, opacity=0.5)
+        circle.set_fill(BLUE, opacity=1)
         circle.set_stroke(BLUE_E, width=4)
         square = Square()
 
