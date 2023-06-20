26384beea3eb6955ef1a2ec42b82f3e6406f07fc
slowisfast <slowisfast2030@163.com>
2023-06-20 12:07:56 +0800

    做减法，极致的专注

--- project_linus/apply_function.py
+++ project_linus/apply_function.py
@@ -20,8 +20,8 @@ class AnimatingMethods(Scene):
         # self.wait()
         self.play(grid.animate.set_submobject_colors_by_gradient(BLUE, GREEN))
         self.wait()
-        self.play(grid.animate.set_height(TAU - MED_SMALL_BUFF))
-        self.wait()
+        #self.play(grid.animate.set_height(TAU - MED_SMALL_BUFF))
+        #self.wait()
 
         # The method Mobject.apply_complex_function lets you apply arbitrary
         # complex functions, treating the points defining the mobject as
