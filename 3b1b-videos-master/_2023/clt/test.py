15e69c2e83f82bca6bd1ac18f21ab78f9eba701f
slowisfast <slowisfast2030@163.com>
2023-06-21 20:50:55 +0800

    做减法，极致的专注

---
+++ 3b1b-videos-master/_2023/clt/test.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/Users/linus/Desktop/less-is-more/3b1b_anaconda_install/manim/3b1b-videos-master')
+# 除了上面这种方式，有没有更加优雅的方式
+
+from manim_imports_ext import *
+from _2023.clt.main import *
