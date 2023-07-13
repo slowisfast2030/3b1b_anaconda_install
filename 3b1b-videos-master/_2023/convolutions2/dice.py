4755410a2683c77fb9a3de92d2a8505f67544f04
slowisfast <slowisfast2030@163.com>
2023-07-13 17:47:56 +0800

    做减法，极致的专注

--- 3b1b-videos-master/_2023/convolutions2/dice.py
+++ 3b1b-videos-master/_2023/convolutions2/dice.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/Users/linus/Desktop/less-is-more/3b1b_anaconda_install/manim/3b1b-videos-master')
+
 from manim_imports_ext import *
 from _2022.convolutions.discrete import *
 from _2023.clt.main import *
