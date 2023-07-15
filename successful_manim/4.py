67098798ca16be675d08d71658e4a14a44443a29
slowisfast <slowisfast2030@163.com>
2023-07-15 18:28:57 +0800

    做减法，极致的专注

--- successful_manim/4.py
+++ successful_manim/4.py
@@ -53,12 +53,12 @@ class AntiDerivative(InteractiveScene):
         # 即使不理解，但是可以模仿这个函数的用法
         def update_area(area: VMobject):
             x = get_x()
-            area.become(high_graph) # 如果manimgl版本的源码不好懂，可以查看manimce版本的实现。manimce版本的实现更加清晰。
+            area.become(high_graph) # 如果manimgl版本的源码不好懂，可以查看manimce版本的实现。manimce版本的实现更加清晰
             area.set_stroke(width=0)
             area.set_fill(BLUE, 0.5)
             # 这一行什么意思？
-            area.pointwise_become_partial( # 很庆幸，在manimce版本中也有实现
-                high_graph, 0, inverse_interpolate(x_min, x_max, x)
+            area.pointwise_become_partial( # 很庆幸，在manimce版本中也有实现。不过没有给示例，所以还是不太清楚
+                high_graph, 0, inverse_interpolate(x_min, x_max, x) # inverse_interpolate()函数的返回结果是一个float
             )
             # 这两行不是很明白。add_line_to()函数是什么意思？有没有办法能够测试一下？
             area.add_line_to(planes[0].c2p(x, 0)) # 这一行不能注释。否则会有一条线倾斜着改变矩形的大小
