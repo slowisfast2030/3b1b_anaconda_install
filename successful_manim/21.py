59f2dffe4cb6d16b5a005b8078c10493e868d779
slowisfast <slowisfast2030@163.com>
2023-07-21 17:28:49 +0800

    做减法，极致的专注

---
+++ successful_manim/21.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('/Users/linus/Desktop/less-is-more/3b1b_anaconda_install/manim/3b1b-videos-master')
+
+from manim_imports_ext import *
+
+class test(InteractiveScene):
+    def construct(self):
+        c = Circle()
+        s = Square()
+        self.add(c)
+        self.play(Transform(c, s))
+        self.wait()
