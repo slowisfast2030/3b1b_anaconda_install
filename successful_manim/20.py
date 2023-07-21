a8e99a75a75b1ac99eea47bc8b3b6376719a9725
slowisfast <slowisfast2030@163.com>
2023-07-21 09:11:51 +0800

    做减法，极致的专注

--- successful_manim/20.py
+++ successful_manim/20.py
@@ -9,43 +9,43 @@ import sympy
 class SumsOfSizeFive(TeacherStudentsScene):
     def construct(self):
         morty = self.teacher
-        stds = self.students
+        #stds = self.students
 
         self.play(
             morty.change("raise_right_hand", self.screen),
-            self.change_students("pondering", "happy", "tease", look_at=self.screen)
+            #self.change_students("pondering", "happy", "tease", look_at=self.screen)
         )
         self.wait()
 
         # Show sums
-        def get_sum():
-            values = [
-                Integer(random.choice([-1, 1]), include_sign=True)
-                for x in range(5)
-            ]
-            lhs = Tex(R"\text{Sum} = ")
-            rhs = Integer(sum(v.get_value() for v in values))
-            result = VGroup(lhs, *values, Tex(R" = "), rhs)
-            result.arrange(RIGHT, buff=0.15)
-            result.next_to(self.screen, RIGHT, buff=-0.5)
-            return result
+        # def get_sum():
+        #     values = [
+        #         Integer(random.choice([-1, 1]), include_sign=True)
+        #         for x in range(5)
+        #     ]
+        #     lhs = Tex(R"\text{Sum} = ")
+        #     rhs = Integer(sum(v.get_value() for v in values))
+        #     result = VGroup(lhs, *values, Tex(R" = "), rhs)
+        #     result.arrange(RIGHT, buff=0.15)
+        #     result.next_to(self.screen, RIGHT, buff=-0.5)
+        #     return result
 
-        curr_sum = get_sum()
+        # curr_sum = get_sum()
 
-        brace = Brace(curr_sum[1:-2], UP)
-        brace_text = brace.get_text("5 values")
-        brace_text.set_color(YELLOW)
+        # brace = Brace(curr_sum[1:-2], UP)
+        # brace_text = brace.get_text("5 values")
+        # brace_text.set_color(YELLOW)
 
-        self.add(curr_sum)
-        self.play(GrowFromCenter(brace), FadeIn(brace_text))
-        for x in range(7):
-            new_sum = get_sum()
-            self.play(
-                FadeOut(curr_sum[1:], lag_ratio=0.1, shift=0.2 * UP),
-                FadeIn(new_sum[1:], lag_ratio=0.1, shift=0.2 * UP),
-                *(pi.animate.look_at(new_sum) for pi in self.pi_creatures)
-            )
-            self.wait()
-            curr_sum = new_sum
+        # self.add(curr_sum)
+        # self.play(GrowFromCenter(brace), FadeIn(brace_text))
+        # for x in range(7):
+        #     new_sum = get_sum()
+        #     self.play(
+        #         FadeOut(curr_sum[1:], lag_ratio=0.1, shift=0.2 * UP),
+        #         FadeIn(new_sum[1:], lag_ratio=0.1, shift=0.2 * UP),
+        #         *(pi.animate.look_at(new_sum) for pi in self.pi_creatures)
+        #     )
+        #     self.wait()
+        #     curr_sum = new_sum
 
-        self.wait()
+        # self.wait()
