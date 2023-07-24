f5da5f1cbbc28a1292c668bcfb8b802c725bdda3
slowisfast <slowisfast2030@163.com>
2023-07-24 20:50:16 +0800

    做减法，极致的专注

---
+++ successful_manim/path.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('/Users/linus/Desktop/less-is-more/3b1b_anaconda_install/manim/3b1b-videos-master')
+
+from manim_imports_ext import *
+
+class test(InteractiveScene):
+    def construct(self):
+        c = Dot()
+        self.add(c)
+        p = TracedPath(c.get_center, stroke_color=RED, time_traced=1)
+        
+        #p = TracingTail(c.get_center, stroke_color=RED)
+
+        self.add(p)
+
+        self.play(c.animate.shift(200 * RIGHT), run_time=20)
