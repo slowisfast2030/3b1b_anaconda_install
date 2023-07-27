b67b82d4797946ff71faf21f5b4cd56a74a04dad
slowisfast <slowisfast2030@163.com>
2023-07-27 19:16:01 +0800

    做减法，极致的专注

---
+++ successful_manim/m.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('/Users/linus/Desktop/less-is-more/3b1b_anaconda_install/manim/3b1b-videos-master')
+
+from manim_imports_ext import *
+
+class test(Scene):
+    def construct(self):
+        m = Matrix(matrix = np.ones((3, 4)))
+        self.add(m)
+        self.wait()
