91efbb92c7a8abb394c524c56d1558348823b211
slowisfast <slowisfast2030@163.com>
2023-07-31 10:38:53 +0800

    做减法，极致的专注

--- udemy/trans.py
+++ udemy/trans.py
@@ -7,9 +7,9 @@ class trans(Scene):
 	def construct(self): 
 		obj1 = Square(fill_opacity=1).set_color(BLUE)
 		obj2 = Circle(fill_opacity=1).set_color(YELLOW).shift(UP*2)
-		#self.play(Transform(obj1, obj2))
-		vm = VMobject()
+		self.play(Transform(obj1, obj2))
+		# vm = VMobject()
 
-		vm.interpolate(obj1, obj2, alpha=0.5)
-		self.add(vm)
-		self.wait(1)
+		# vm.interpolate(obj1, obj2, alpha=0.5)
+		# self.add(vm)
+		# self.wait(1)
