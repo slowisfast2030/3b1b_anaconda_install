1451ee6ab7ff015fe696fd5654c2367794af8cdf
slowisfast <slowisfast2030@163.com>
2023-08-02 19:58:49 +0800

    做减法，极致的专注

--- manimlib/animation/update.py
+++ manimlib/animation/update.py
@@ -12,11 +12,15 @@ if TYPE_CHECKING:
 
 """
 rect = Rectangle().set_color(BLUE)
-ball = Dot().set_color(RED)
+ball_1 = Dot().set_color(RED)
+ball_2 = Dot().set_color(YELLOW)
 self.play(
     ShowCreation(rect, run_time=2),
-    UpdateFromFunc(ball, lambda m: m.move_to(rect.get_end()))
+    UpdateFromFunc(ball_1, lambda m: m.move_to(rect.get_end())),
+    ball_2.animate.move_to(rect.get_end())              
 )
+
+
 """
 class UpdateFromFunc(Animation):
     """
--- udemy/Entry_Display_Exit_Animations/test.py
+++ udemy/Entry_Display_Exit_Animations/test.py
@@ -122,10 +122,10 @@ class test9(Scene):
 class test10(Scene):
     def construct(self):
         rect = Rectangle().set_color(BLUE)
-        ball = Dot().set_color(RED)
-        ball1 = Dot().set_color(YELLOW)
+        ball_1 = Dot().set_color(RED)
+        ball_2 = Dot().set_color(YELLOW)
         self.play(
             ShowCreation(rect, run_time=2),
-            UpdateFromFunc(ball, lambda m: m.move_to(rect.get_end())),
-            #ball1.animate.move_to(rect.get_end())              
+            UpdateFromFunc(ball_1, lambda m: m.move_to(rect.get_end())),
+            ball_2.animate.move_to(rect.get_end())              
         )
