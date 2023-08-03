46f7888d6d4ca3ecc79605e8a514c6c9dff38c00
slowisfast <slowisfast2030@163.com>
2023-08-03 11:59:40 +0800

    做减法，极致的专注

--- udemy/Mobjects/test.py
+++ udemy/Mobjects/test.py
@@ -35,6 +35,82 @@ class test(Scene):
 		self.wait(3)
 		
 		
+class test1(Scene):
+	
+	def construct(self): 
+		text = Text("3D Graphs")
+		self.play(FadeIn(text))
+		#self.wait(3)
+		self.play(FadeOut(text))
+
+		#Scene Material
+		title = Text("3D Graphs").shift(UP*3.5)
+		self.play(FadeIn(title))
+
+		#stuff in scene
+		obj1 = Cube().shift(LEFT*4 + IN*4).set_color(GREEN)
+		obj2 = Prism().shift(RIGHT*3 + UP*1 + OUT*1).set_color(RED)
+		obj3 = Sphere().shift(DOWN*2).set_color(BLUE)
+		self.play(FadeIn(Group(obj1, obj2, obj3)))
+
+		#Camera
+		frame = self.camera.frame
+
+		frame2 = frame.copy()
+		frame2.set_euler_angles(
+			theta = -10*DEGREES,
+			phi = 50 *DEGREES
+		)
+		self.play(Transform(frame, frame2))
+
+		def rotateScene():
+			for i in range(200):
+				frame.increment_theta(0.01)
+				self.wait(0.001)
+				
+
+
+		axes3d = ThreeDAxes(x_range=(-5, 5, 1), 
+		      				y_range=(-5, 5, 1),
+							height = 12,
+							width = 12,
+							#stroke_color = WHITE,
+							#stroke_width =  4,
+							#include_tip = True,
+							#include_ticks = True,
+							)
+		
+		self.play(FadeIn(axes3d))
+		
+		rotateScene()
+
+		
+		func2 = lambda q: [np.sin(q), np.cos(q), q]
+
+		func3 = lambda t: np.array([
+			5*np.cos(t)*np.cos(4*t)*np.cos(t)**9,
+			5*np.sin(t)*np.cos(4*t)*np.cos(0.5)**9,
+			5*np.sin(0.5)*np.cos(4*t)*np.cos(0.5)**8])
+		
+		graph = axes3d.get_parametric_curve(
+					  func2, 
+				      color=BLUE, 
+					  #step_size=0.001, 
+					  t_range=[-10, 10, 0.01]
+					  )
+		self.play(FadeIn(graph))
+		rotateScene()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
